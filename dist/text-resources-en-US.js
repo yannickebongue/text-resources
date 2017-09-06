01cf20197fd1f25ce3cb8354f300af0864e274dd
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-09-06 13:17:31 +0200

    Fix bower package
    - Include the `dist` directory onto git repo
    - Exclude the `package.json` file from bower package

---
+++ dist/text-resources-en-US.js
@@ -0,0 +1,28 @@
+/*! text-resources | 0.1.2 | 2017-09-06 */
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        global.CurrencyNames = module.require("./currency-names");
+        module.exports = factory(global);
+    } else {
+        factory(global);
+    }
+})(this, function(global) {
+    global.CurrencyNames["en-US"] = {
+        USD: "$"
+    };
+    return global.CurrencyNames;
+});
+
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        global.FormatData = module.require("./format-data");
+        module.exports = factory(global);
+    } else {
+        factory(global);
+    }
+})(this, function(global) {
+    global.FormatData["en-US"] = {
+        NumberPatterns: [ "#,##0.###;-#,##0.###", "\xa4#,##0.00;(\xa4#,##0.00)", "#,##0%" ]
+    };
+    return global.FormatData;
+});
---
+++ dist/text-resources-es-ES.js
@@ -0,0 +1,44 @@
+/*! text-resources | 0.1.2 | 2017-09-06 */
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        global.CalendarData = module.require("./calendar-data");
+        module.exports = factory(global);
+    } else {
+        factory(global);
+    }
+})(this, function(global) {
+    global.CalendarData["es-ES"] = {
+        firstDayOfWeek: "2",
+        minimalDaysInFirstWeek: "4"
+    };
+    return global.CalendarData;
+});
+
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        global.CurrencyNames = module.require("./currency-names");
+        module.exports = factory(global);
+    } else {
+        factory(global);
+    }
+})(this, function(global) {
+    global.CurrencyNames["es-ES"] = {
+        ESP: "Pts",
+        EUR: "\u20ac"
+    };
+    return global.CurrencyNames;
+});
+
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        global.FormatData = module.require("./format-data");
+        module.exports = factory(global);
+    } else {
+        factory(global);
+    }
+})(this, function(global) {
+    global.FormatData["es-ES"] = {
+        NumberPatterns: [ "#,##0.###;-#,##0.###", "#,##0 \xa4;-#,##0 \xa4", "#,##0%" ]
+    };
+    return global.FormatData;
+});
